c591200af0de94234671f6cf2424407580daaa8f
Jeff <jeff@jrm.cc>
2024-10-13 17:52:16 -0700

    Moving all the commented out to bottom

--- stats.py
+++ stats.py
@@ -1,4 +1,46 @@
 prstats = [
+    {
+        "name": "Controller Temperature (°C)",
+        "address": 259,
+        "scale": 1,
+        "isSigned": True,
+        "type": "independent",
+        "description": "Base plate temperature"
+    },
+
+    {
+        "name": "Motor Temperature (°C)",
+        "address": 261,
+        "scale": 1,
+        "type": "independent",
+        "description": "Motor temperature "
+    },
+    {
+        "name": "Motor Current (A)",
+        "address": 262,
+        "scale": 32,
+        "type": "independent",
+        "description": "Motor peak current ",
+        "round": 2
+    },
+    {
+        "name": "Motor RPM (RPM)",
+        "address": 263,
+        "scale": 1,
+        "isSigned": True,
+        "type": "independent",
+        "description": "Motor speed "
+    },
+
+    # The CA does NOT report this, and I think it's nice to have (even if it's just VxA)
+    {
+        "name": "Battery Power (W)",
+        "address": 268,
+        "scale": 1,
+        "isSigned": True,
+        "type": "independent",
+        "description": "Calculated battery output power"
+    },
 
     {
         "name": "Faults (text)",
@@ -26,98 +68,6 @@ prstats = [
         "description": "15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0\n\n1=active 0=not active\n\nbit  0: Controller over voltage (flash code 1,1)\nbit  1: Phase over current (flash code 1,2)\nbit  2: Current sensor calibration (flash code 1,3)\nbit  3: Current sensor over current (flash code 1,4)\nbit  4: Controller over temperature (flash code 1,5)\nbit  5: Motor Hall sensor fault (flash code 1,6)\nbit  6: Controller under voltage (flash code 1,7)\nbit  7: POST static gating test (flash code 1,8)\nbit  8: Network communication timeout (flash code 2,1)\nbit  9: Instantaneous phase over current (flash code 2,2)\nbit 10: Motor over temperature (flash code 2,3)\nbit 11: Throttle voltage outside range (flash code 2,4)\nbit 12: Instantaneous controller over voltage (flash code 2,5)\nbit 13: Internal error (flash code 2,6\nbit 14: POST dynamic gating test (flash code 2,7)\nbit 15: Instantaneous under voltage (flash code 2,8)"
     },
 
-    {
-        "name": "Controller Temperature (°C)",
-        "address": 259,
-        "scale": 1,
-        "isSigned": True,
-        "type": "independent",
-        "description": "Base plate temperature"
-    },
-
-    ## This relies on the wheel diameter being set, and accurate, in the PR suite
-    ## Since most folks set wheel size on the CA (because it does it's own speed calc)
-    ## this is likely to be set wrong (default 24") or not tweaked to match what's
-    ## being seen on the CA. Thus we'll not collect it.
-    # {
-    #     "name": "Vehicle Speed (km/h)",
-    #     "address": 260,
-    #     "scale": 256,
-    #     "isSigned": True,
-    #     "type": "independent",
-    #     "description": "Calculated vehicle speed"
-    # },
-
-    {
-        "name": "Motor Temperature (°C)",
-        "address": 261,
-        "scale": 1,
-        "type": "independent",
-        "description": "Motor temperature "
-    },
-    {
-        "name": "Motor Current (A)",
-        "address": 262,
-        "scale": 32,
-        "type": "independent",
-        "description": "Motor peak current ",
-        "round": 2
-    },
-    {
-        "name": "Motor RPM (RPM)",
-        "address": 263,
-        "scale": 1,
-        "isSigned": True,
-        "type": "independent",
-        "description": "Motor speed "
-    },
-
-    ## This one routinely reports speeds over 100%
-    # {
-    #     "name": "Motor Speed (%)",
-    #     "address": 264,
-    #     "scale": 40.96,
-    #     "type": "independent",
-    #     "description": "Motor speed"
-    # },
-
-    ## This works just fine, but it's redundant with what the CA reports
-    # {
-    #     "name": "Battery Voltage (V)",
-    #     "address": 265,
-    #     "scale": 32,
-    #     "type": "independent",
-    #     "description": "Measured battery voltage"
-    # },
-
-    ## Also works just fine, but it's redundant with what the CA reports
-    # {
-    #     "name": "Battery Current (A)",
-    #     "address": 266,
-    #     "scale": 32,
-    #     "isSigned": True,
-    #     "type": "independent",
-    #     "description": "Measured battery amperage"
-    # },
-
-    ## This one did not work right at all.
-    # {
-    #     "name": "Battery State of Charge (%)",
-    #     "address": 267,
-    #     "scale": 32,
-    #     "type": "independent",
-    #     "description": "Remaining battery capacity"
-    # },
-
-    # The CA does NOT report this, and I think it's nice to have (even if it's just VxA)
-    {
-        "name": "Battery Power (W)",
-        "address": 268,
-        "scale": 1,
-        "isSigned": True,
-        "type": "independent",
-        "description": "Calculated battery output power"
-    },
     {
         "name": "Last Fault (text)",
         "address": 269,
@@ -167,6 +117,56 @@ prstats = [
         "description": "Filtered brake 2 voltage",
         "round": 2
     },
+
+    ## This relies on the wheel diameter being set, and accurate, in the PR suite
+    ## Since most folks set wheel size on the CA (because it does it's own speed calc)
+    ## this is likely to be set wrong (default 24") or not tweaked to match what's
+    ## being seen on the CA. Thus we'll not collect it.
+    # {
+    #     "name": "Vehicle Speed (km/h)",
+    #     "address": 260,
+    #     "scale": 256,
+    #     "isSigned": True,
+    #     "type": "independent",
+    #     "description": "Calculated vehicle speed"
+    # },
+
+    ## This one routinely reports speeds over 100%
+    # {
+    #     "name": "Motor Speed (%)",
+    #     "address": 264,
+    #     "scale": 40.96,
+    #     "type": "independent",
+    #     "description": "Motor speed"
+    # },
+
+    ## This works just fine, but it's redundant with what the CA reports
+    # {
+    #     "name": "Battery Voltage (V)",
+    #     "address": 265,
+    #     "scale": 32,
+    #     "type": "independent",
+    #     "description": "Measured battery voltage"
+    # },
+
+    ## Also works just fine, but it's redundant with what the CA reports
+    # {
+    #     "name": "Battery Current (A)",
+    #     "address": 266,
+    #     "scale": 32,
+    #     "isSigned": True,
+    #     "type": "independent",
+    #     "description": "Measured battery amperage"
+    # },
+
+    ## This one did not work right at all.
+    # {
+    #     "name": "Battery State of Charge (%)",
+    #     "address": 267,
+    #     "scale": 32,
+    #     "type": "independent",
+    #     "description": "Remaining battery capacity"
+    # },
 
     ## The phase information, while neat, just isn't that useful.
     ## Maybe if polled MUCH faster, you could make a neat wave diagram
